db98ccf36abc73bb6da67f906490a46eb5e886a4
ntl <nathanladd+github@gmail.com>
2015-10-14 10:46:53 -0500

    Swap second and third octets

--- lib/controls/id.rb
+++ lib/controls/id.rb
@@ -9,7 +9,7 @@ module Controls
 
       third_prefix = ['8', '9', 'A', 'B'].sample
 
-      "#{first_octet}-4000-#{third_prefix}000-0000-000000000000"
+      "#{first_octet}-0000-4000-#{third_prefix}000-000000000000"
 
       # NOTE This should become: Identifier::UUID::Controls::Incrementing.example(i) [Scott, Mon Oct 12 2015]
     end
