531b722da80e11089f52a61b471711a2fecdb596
Scott Bellware <sbellware@gmail.com>
2016-02-05 13:27:26 -0600

    Time control provides raw time as well as ISO8601

--- lib/controls/time.rb
+++ lib/controls/time.rb
@@ -3,8 +3,20 @@ require 'controls'
 module Controls
   module Time
     def self.example
-      Clock::UTC.iso8601(::Time.utc(2000))
+      ISO8601.example
     end
     def self.reference; example; end
+
+    module ISO8601
+      def self.example
+        Clock::UTC.iso8601(Raw.example)
+      end
+    end
+
+    module Raw
+      def self.example
+        ::Time.utc(2000)
+      end
+    end
   end
 end
