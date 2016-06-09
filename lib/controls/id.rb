1b433b694dd94498993baa123cb5f41bff0d7d83
Scott Bellware <sbellware@gmail.com>
2016-06-08 22:10:38 -0700

    The ID control signature matches the Identifier::UUID::Incrementing control signature (the increment named arg is included)

--- lib/controls/id.rb
+++ lib/controls/id.rb
@@ -2,8 +2,8 @@ require 'controls'
 
 module Controls
   module ID
-    def self.get(i=nil, sample: nil)
-      Identifier::UUID::Controls::Incrementing.example(i, sample: sample)
+    def self.get(i=nil, increment: nil, sample: nil)
+      Identifier::UUID::Controls::Incrementing.example(i, increment: increment, sample: sample)
     end
   end
 end
